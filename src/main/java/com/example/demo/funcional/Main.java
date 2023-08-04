8cc629eed7a5068a09b0fedf6209cb4afdb677df
levalladares <levalladares@uce.edu.ec>
2023-08-04 00:52:31 -0500

    Tarea16

--- src/main/java/com/example/demo/funcional/Main.java
+++ src/main/java/com/example/demo/funcional/Main.java
@@ -1,5 +1,6 @@
 package com.example.demo.funcional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,165 +13,26 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Persona per = new PersonaImpl();
-		per.caminar();
 		
-		//1.- SUPPLIER
-		//Clases:
-		IPersonaSupplier<String> supplier1= new PersonaSupplierImpl();
-		//buena practica para imprimir es un loger (slf4j)
-		LOG.info("Supplier clase:"+supplier1.getId() );
-		//Lambdas:
-		IPersonaSupplier<String> supplier2 = () ->  "17239737966";
-		LOG.info("Supplier lambda:"+supplier2.getId() );
-		//EJEMPLO2
-		IPersonaSupplier<String> supplier3 = () ->  {
-			String cedula="17239737966";
-			cedula= cedula+"zzzzzzzzzz";
-			return cedula;
-		};
-		LOG.info("Supplier lambda 2:"+supplier3.getId() );
-		//METODOS REFERENCIADOS
-		MetodosReferenciados metodos= new MetodosReferenciados();
-		IPersonaSupplier<Integer> supplier4=MetodosReferenciados::getId;  //--> dos puntos se conoce como metodos referenciados
-		LOG.info("Supplier metodoReferenciado:"+supplier4.getId() );
-		
-		
-		
-		
-		//2.- CONSUMER
-		//Clase
-		IPersonaConsumer<String> consumer1= new PersonaConsumerImpl();
-		LOG.info("Consumer clase:" );
-		consumer1.accept("Luis Valladares");
-		//Lambda
-		IPersonaConsumer<String> consumer2= cadena->{
-			LOG.info("HOLA");
-			LOG.info(cadena);
-			LOG.info("CHAO");
-			};
-		LOG.info("Consumer LABMDA:" );
-		consumer2.accept("Luis Valladares2");
-		//metodos referenciados
-		IPersonaConsumer<String> consumer3 = MetodosReferenciados::acceptar;
-		LOG.info("Consumer metodos referenciados:" );
-		consumer3.accept("A");
-		
-		
-		
-		
-		//3.-PREDICATE
-		//Lambda
-		//ejemplo1
-		IPersonaPredicate<Integer> predicate1 = valor -> valor.compareTo(8)==0;
-		LOG.info("Predicate lambda:"+predicate1.evaluar(4) );
-		// ejemplo2
-		IPersonaPredicate<Integer> predicate2 = valor -> {
-			Integer valor2=10;
-			valor=valor+valor2;
-			if(valor.compareTo(100)>0) {
-				return true;
-			}else {
-				return false;
-			}
-		};
-		LOG.info("Predicate lambda:"+predicate2.evaluar(95));
-		//ejemplo3
-		IPersonaPredicate<String> predicate3 = letra ->"Luis".contains(letra);
-		LOG.info("Predicate ejemplo lambda:"+predicate3.evaluar("z") );
-		//ejemplo14
-		IPersonaBiPredicate<String,String> predicate4 = (letra,cadena) ->cadena.contains(letra); 
-		LOG.info("Predicate lambda4:"+predicate4.evaluar("z","Luis") );
-		//metodos referenciales
-		IPersonaPredicate<String> predicate5 = metodos::evaluar;
-		LOG.info("predicate metodoReferenciado:"+predicate5.evaluar("Jose") );
-		
-		
-		
-		//4.-FUNCTION
-		//ejemplo1
-		IPersonaFunction<String, Integer> function= numero ->numero.toString();
-		LOG.info("FUNCTION lambda:"+function.aplicar(8) );
-		//ejemplo2
-		IPersonaFunction<String, Integer> function2= numero ->{
-			String valorFinal = numero.toString().concat(" valor");
-			return valorFinal;
-		};
-		LOG.info("FUNCTION lambda2:"+function2.aplicar(10) );
-		//metodos referenciados
-		IPersonaFunction<Integer, String >  function3 = metodos::aplicar;
-		LOG.info("predicate metodoReferenciado:"+function3.aplicar("Ahorros") );
-		
-		
-		//5.-UnaryOperator
-		IPersonaUnary<Integer> unary=numero -> numero+(numero*2);
-		LOG.info("UNARY lambda:"+unary.aplicar(3) );
-		//CON EL FUNCTION
-		IPersonaUnaryFunction<Integer> unary2=numero -> numero+(numero*2);
-		LOG.info("UNARY lambda2:"+unary2.aplicar(3) );
-		//metodos referenciados 
-		IPersonaUnary<Integer>  unary3 = metodos:: aplicar;
-		LOG.info("UNARI METODOS REFERENCIADOS:"+ unary3.aplicar(5));
-		
-		
-		
-		//>>>>>>>>>>>>>>>>>>>>METODOS HIGH ORDER<<<<<<<<<<<<<<<<<
-		
-		//SUPPLIER
-		MetodosHighOrder highOrder = new MetodosHighOrder();
-		//1. Clase
-		IPersonaSupplier<String> supplierHO= new PersonaSupplierImpl();
-		highOrder.metodo(supplierHO);
-		//2. Lambdas
-		MetodosHighOrder.metodo( () ->  "17239737966HO");
-		//3. Metodos Referenciados
-		MetodosHighOrder.metodo(MetodosReferenciados::getIdHO);
-		
-		
-		
-		//CONSUMER
-		//1.clase
-		MetodosHighOrder.metodo2(new PersonaConsumerImpl(), "clase consumer");
-		//2.Lambdas
-		MetodosHighOrder.metodo2(cadena->{
-			LOG.info("HOLA");
-			LOG.info(cadena);
-			LOG.info("CHAO Ho");
-			}, "Consumer LABMDA:");
-		//3.metodos referenciados
-		MetodosHighOrder.metodo2(MetodosReferenciados::acceptar,"hola");
-	
-		
-		
-		
-		// >> INTERFACES FUNCIONALES JAVA
+		//>>>>>>>>>>>>>>>>DEBER<<<<<<<<<<<<<<<<<<<
 		//1.Supplier
-		Stream<String> lista=Stream.generate(() ->  "17239737966").limit(10);
-		lista.forEach(cadena-> LOG.info(cadena));
-		
+		Stream<String> listaA=Stream.generate(MetodosReferenciados::A).limit(5);
+		listaA.forEach(cadena-> LOG.info(cadena));
 		//2.Consumer
-		List<Integer> listaNumeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13);
-		listaNumeros.forEach(cadena->LOG.info(""+cadena));
-		
+		List<String> listaB= Arrays.asList("Jose","Andrea","Flavio","Jose","Csrlos");
+		listaB.forEach(MetodosReferenciados::B);
 		//3.Predicate
-		Stream<Integer> listaFinal=listaNumeros.stream().filter(numero -> numero>=5);
-		listaFinal.forEach(numero -> LOG.info("Valor: "+numero));
-		
-		//4.Function
-		Stream <String>listaCambiada=listaNumeros.stream().map(numero -> {
-			Integer num=10;
-			num=numero+num;
-			return "N:"+num;
-		});
-		listaCambiada.forEach(cadena -> LOG.info(cadena));
-		
+		List<String> listaC= Arrays.asList("Luis ernesto Valladares bastidas".split(" "));
+		listaC.forEach(MetodosReferenciados::C);
+		//4. Function
+		listaC.forEach(MetodosReferenciados::D);
 		//5.UnaryOperator
-		Stream <Integer>listaCambiada2=listaNumeros.stream().map(numero -> {
-			Integer num=10;
-			num=numero+num;
-			return num;
-		});
-		listaCambiada2.forEach(cadena -> LOG.info(cadena.toString()));
+		List<Integer>listaD=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		listaD.forEach(MetodosReferenciados::E);
+
+		
+		
+	
 	}
 
 
--- src/main/java/com/example/demo/funcional/MetodosReferenciados.java
+++ src/main/java/com/example/demo/funcional/MetodosReferenciados.java
@@ -36,4 +36,47 @@ public class MetodosReferenciados {
 		nuevo=arg+100;
 		return nuevo;
 	}
+	
+	//>>>>>>>>> METODOS REFERENCIADOS TAREA 16 <<<<<<<<<<<<<
+	//SUPPLIER
+	public static String A() {
+		return "Ejemplo 1";
+		//LOG.info("Ejemplo 1:"+arg);
+	}
+	//CONSUMER
+	//metodo que devuelve las palabras en mayusculas
+	public static void B(String arg) {
+		LOG.info("Ejemplo 2:"+arg.toUpperCase());
+	}
+	//PREDICATE
+	//metodo que devuelve solo la palabras que comienzan con mayusculas
+	public static boolean C (String arg) {
+		char primerCaracter = arg.charAt(0);
+		if(Character.isUpperCase(primerCaracter)) {
+			LOG.info("Ejemplo 3:"+arg);
+			return true;
+		}
+		return false;
+	}
+	//FUNCTION
+	//metodo que devuelve el primer caracter de cada palabra
+	public static Character D(String arg) {
+		Character caracter=arg.charAt(0);
+		LOG.info("Ejemplo 4:"+caracter);
+		return caracter;
+	}
+	//UnaryOperator
+	//metodo que devuelve solo los numeros pares 
+	public static Integer E(Integer arg) {
+		Integer par=null;
+		if(arg%2 ==0) {
+			par = arg;
+			LOG.info("ejemplo 5:"+ par);
+		}
+		return par;
+	}
+	
+	
+	
 }
+
